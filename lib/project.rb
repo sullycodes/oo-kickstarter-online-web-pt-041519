bbcb64a9f940f1813b63f204ebd2896e40b2ea93
Daniel Sullivan <dansullivan19@gmail.com>
2019-05-25 14:23:04 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -13,18 +13,3 @@ class Project
   end
   
 end
-
-# class Backer
-  
-#   attr_reader :name, :backed_projects
-  
-#   def initialize(name)
-#     @name = name
-#     @backed_projects = []
-#   end
-  
-#   def back_project(project)
-#     @backed_projects << project
-#   end
-  
-# end
